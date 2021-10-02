c1ffdc0738e8a4d20da00a110e89c57cf1abe463
iaroslav.sviatkin <iaroslav.sviatkin.globallogic.com>
2021-10-02 10:13:52 +0300

    commit2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,6 +13,9 @@ public class Main {
         System.out.println("hi");
         System.out.println("hi");
         System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
 
     }
 }
